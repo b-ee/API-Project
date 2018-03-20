e66714e626f1f98dceb83426021217d915853117
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-03-20 15:01:18 -0400

    BH before form manipulation

--- emailForm.js
+++ emailForm.js
@@ -1,17 +1,13 @@
-// const emailForm = document.querySelector("form");
-// const userEmail = document.querySelector(".userEmail");
+const emailForm = document.querySelector("form");
+const userEmail = document.querySelector(".userEmail");
 
-// emailForm.addEventListener("submit",addressSwitch);
-// emailForm.addEventListener("load",function(e){
-//   console.log(userEmail.value);
-//   alert("Form sent!");
-// });
+emailForm.addEventListener("submit",addressSwitch);
 
-// function addressSwitch(e) {
-//   e.preventDefault();
-//   console.log(userEmail.value);
+function addressSwitch(e) {
+  e.preventDefault();
+  console.log(userEmail.value);
 
-//   emailForm.action = `https://formspree.io/${userEmail.value}`
+  emailForm.action = `https://formspree.io/${userEmail.value}`
 
-//   emailForm.open("POST",`https://formspree.io/${userEmail.value}`);
-// }
+  emailForm.open("POST",`https://formspree.io/${userEmail.value}`);
+}
