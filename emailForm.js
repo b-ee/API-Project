a1715492c6ecbfd028752b18e39fe399fa6ad550
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-03-21 11:58:58 -0400

    BH hosting to Firebase

--- emailForm.js
+++ emailForm.js
@@ -3,8 +3,6 @@ let emailForm = document.getElementById("emailForm");
 submitBtn.addEventListener("click", addressSwitch);
 
 function addressSwitch(e) {
-  e.preventDefault();
-  
   const email = document.getElementById("email");
 
   emailForm.action = `https://formspree.io/${email.value}`;
